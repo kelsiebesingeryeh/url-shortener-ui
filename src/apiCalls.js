cb4dce2a7bcfac586797b2b34349aec1d801ec4f
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-09 09:43:18 -0700

    Add post request function to apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,4 +3,18 @@ export const getUrls = () => {
       .then(response => response.json())
 }
 
+export const createNewURL = (newURL) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newURL)
+  }
+  return (
+    fetch("http://localhost:3001/api/v1/urls")
+    .then(response => response.json())
+  )
+}
+
 // add the post request here

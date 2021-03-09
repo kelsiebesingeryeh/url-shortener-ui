9ec7d046b6c95d4f5c1b2c28ff6f89f291b1411b
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-09 11:07:10 -0700

    Add second url to test

--- cypress/integration/url_shortener_spec.js
+++ cypress/integration/url_shortener_spec.js
@@ -1,37 +1,64 @@
-describe("URL Shortener", () => {
+describe.only("URL Shortener", () => {
     beforeEach(() => {
-      const baseURL = "http://localhost:3000/";
-      cy.visit(baseURL);
-    });
+        const baseURL = "http://localhost:3000/"
+        cy.fixture("testURLData.json").then((urlData) => {
+          cy.intercept(
+            "GET",
+            "http://localhost:3001/api/v1/urls",
+            urlData
+          )
+        })
+        cy.visit(baseURL)
+    })
 
-    it("Should allow a user to see the homepage title and exisiting shortened URLS", () => {
+    it.only("Should allow a user to see the homepage title and existing shortened URLS", () => {
       cy.get("h1")
         .should("contain", "URL Shortener")
         .get(".url")
         .should("be.visible")
         .first()
-        .get("h3")
-        .should("contain", "Awesome photo")
-        .get("a")
-        .should("contain", "http://localhost:3001/useshorturl/1")
-        .get("p")
+        .children()
+        .first()
+        .should("contain", "Happy Cat")
+        .next()
+        .should("contain", "http://localhost:3001/useshorturl/100")
+        .next()
         .should(
           "contain",
-          "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+          "https://images.unsplash.com/photo-1591860455878-5458e029601e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+        )
+        .get(".url")
+        .should("be.visible")
+        .first()
+        .next()
+        .children()
+        .first()
+        .should("contain", "happy dogs")
+        .next()
+        .should("contain", "http://localhost:3001/useshorturl/101")
+        .next()
+        .should(
+          "contain",
+          "https://images.unsplash.com/photo-1601758003630-df669e4e825a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
         )
     })
 
     it('should allow a user to be able to view a form with the proper inputs', () => {
         cy.get("form input[name=title]")
           .should("be.visible")
+          .get("form input[name=urlToShorten]")
+          .should("be.visible")
+          .get("button")
+          .should("contain", "Shorten Please!")
+    })
+
+    it('should allow a user to fill out the form and see their information reflected in the input', () => {
+        cy.get("form input[name=title]")
           .type("happy goat")
           .should("have.value", "happy goat")
           .get("form input[name=urlToShorten]")
-          .should("be.visible")
           .type("https://unsplash.com/photos/xB0e8bDV4ww")
           .should("have.value", "https://unsplash.com/photos/xB0e8bDV4ww")
-          .get("button")
-          .should("contain", "Shorten Please!")
     })
   })
 
